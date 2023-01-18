7177c8c4b23d1eac9caaa5a21c173e8c8ef25a97
Inotart <art@mcppl.cn>
2023-01-19 02:04:32 +0800

    重制初始化启动说明

--- __init__.py
+++ __init__.py
@@ -1,1 +1,14 @@
 # DotCS 社区版的主入口文件
+import os
+import json
+import platform
+import ctypes
+
+import rich
+
+import dotcs_ex
+
+rich.print("[green]DotCS 社区版[/]")
+rich.print("[green]作者:万载县互联网服务工作室[/]")
+rich.print(f"[yellow]版本:{dotcs_ex.date.version}[/]")
+
