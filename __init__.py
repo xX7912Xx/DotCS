51be9d79e292ad36040de90fe4236dcd7f051ed9
Inotart <art@mcppl.cn>
2023-01-19 13:36:16 +0800

    配置文件位置修改

--- __init__.py
+++ __init__.py
@@ -5,8 +5,8 @@ import platform
 import ctypes
 
 import rich
-import dynaconf
 
+from config import settings
 try:
     import dotcs_ex
 except:
@@ -18,5 +18,7 @@ rich.print("[green]作者:万载县互联网服务工作室[/]")
 rich.print(f"[yellow]版本:{dotcs_ex.date.version}[/]")
 
 # 登录 DotCS 用户中心
+pass
 
-
+# 初始化配置文件
+rich.print("[yellow]租赁服号:[/]",settings.server,sep="")
